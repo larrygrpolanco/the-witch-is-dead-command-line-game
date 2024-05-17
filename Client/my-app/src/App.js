5fcd3af2f57b0b851b743dbc28fa212900f97f77
Larry Grullon-Polanco <larrygrpolanco@gmail.com>
2024-05-16 21:23:08 -0400

    ...

---
+++ Client/my-app/src/App.js
@@ -0,0 +1,14 @@
+import Sidebar from './Sidebar';
+import ChatArea from './ChatArea';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <Sidebar />
+        <ChatArea />
+    </div>
+  );
+}
+
+export default App;
